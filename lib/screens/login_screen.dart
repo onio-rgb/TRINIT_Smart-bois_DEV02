e74b58f743216518bac1abeb55438ea36433f2d1
onio <astitvastu@gmail.com>
2022-01-29 16:03:35 +0530

    welcome screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late String email;
+  late String password;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../components/Button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Button(
+              title: 'Log In',
+              color: Colors.grey.withOpacity(0.5),
+              onPressed: () {},
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            Button(
+              title: 'Register',
+              color: Colors.grey.withOpacity(0.5),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
